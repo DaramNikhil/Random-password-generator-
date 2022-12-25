d5cef5bc7c8c87fa29825ce0cf44108021d21aef
Daram Nikhil <117379132+DaramNikhil@users.noreply.github.com>
2022-12-25 15:47:41 +0530

    Add files via upload

---
+++ random password genarator.py
@@ -0,0 +1,41 @@
+import tkinter
+from tkinter import *
+import random
+
+tk=tkinter.Tk()
+tk.title('RANDOM-PASSWORD GENARATOR')
+tk.geometry('700x700')
+
+def clear():
+    eent.delete(0,END)
+
+def function():
+    num='1234567890' 
+    lettup='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lettsm='abcdefghijklmnopqrstuvwxyz'
+    symbol='€¥$¢'
+    total=num+lettup+lettsm+symbol
+    len=12
+    pas=''.join(random.sample(total,len))
+    eent.insert(0,pas)
+      
+               
+    
+    
+    
+    
+    
+
+eent=Entry(tk,width=20,relief=RIDGE, justify=CENTER)
+eent.grid(row=0,column=0,padx=100,pady=50)
+
+
+but=Button(tk,text='RANDOM',font=('arial',7),relief=RAISED,bd=5,command=function)
+but.grid(row=1,column=0,padx=150,pady=50)
+
+
+cl=Button(tk,text='CLEAR',font=('arial',7),relief=RAISED,bd=5,command=clear)
+cl.grid(row=2,column=0,padx=150,pady=50)
+
+
+tk.mainloop()
